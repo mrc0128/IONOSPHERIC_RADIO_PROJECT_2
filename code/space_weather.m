4dfcdd887b7fc6d9a1b59c426cb9ce1d9da0aa81
mrc0128 <mrc0128@auburn.edu>
2026-03-16 15:43:08 -0500

    adding printing for json to check data

--- code/space_weather.m
+++ code/space_weather.m
@@ -1,3 +1,5 @@
+clear; close all; clc;
+
 websave('mag_7day.json',     'https://services.swpc.noaa.gov/products/solar-wind/mag-7-day.json');
 websave('plasma_7day.json',  'https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json');
 websave('kp_index.json',     'https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json');
@@ -12,7 +14,7 @@ websave('solar_indices.txt', 'https://services.swpc.noaa.gov/text/daily-solar-in
 %    disp(data{i});
 %end 
 
-%read raw file data and decode json format
+%parse and load file data 
 
 mag_raw = fileread('mag_7day.json');
 plasma_raw = fileread('plasma_7day.json');
@@ -21,5 +23,19 @@ kp_raw = fileread('kp_index.json');
 mag_data = jsondecode(mag_raw);
 plasma_data = jsondecode(plasma_raw);
 kp_data = jsondecode(kp_raw);
+
+%print row 2
+%row = mag_data{2};
+%disp(row)
+
+%print rows 2-6
+for i = 2:6
+    row = mag_data{i};
+    fprintf('Time: %s Bx: %s Bz: %s Bt: %s\n', row{1}, row{2}, row{4}, row{7})
+end
+
+
+
+
 
 
